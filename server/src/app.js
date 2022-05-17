eda0ec2a089418625e9f0d4ee55fd4f704ebb82e
vitoorsouzaa1 <vitor.souza407@yahoo.com.br>
2022-05-17 11:28:42 -0300

    feat: added abort mission funcionality

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
 
 const app = express();
 
@@ -11,11 +13,16 @@ app.use(
     origin: 'http://localhost:3000',
   })
 );
+
+app.use(morgan('combined'));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use(planetsRouter);
-app.get('/', (req, res) => {
+app.use('/planets', planetsRouter);
+app.use('/launches', launchesRouter);
+
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -39,7 +39,12 @@ function loadPlanetsData() {
       });
   });
 }
+
+function getAllPlanets() {
+  return habitablePlanets;
+}
+
 module.exports = {
   loadPlanetsData,
-  planets: habitablePlanets,
+  getAllPlanets,
 };
