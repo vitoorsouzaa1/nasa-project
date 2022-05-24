946d3cce3d8da45ffd5ef39626665ad850e964ff
vitoorsouzaa1 <vitor.souza407@yahoo.com.br>
2022-05-23 21:45:04 -0300

    feat:added pagination to SpaceX API and mapped all data

--- server/src/app.js
+++ server/src/app.js
@@ -3,10 +3,9 @@ const cors = require('cors');
 const path = require('path');
 const morgan = require('morgan');
 
-const planetsRouter = require('./routes/planets/planets.router');
-const launchesRouter = require('./routes/launches/launches.router');
-
 const app = express();
+
+const api = require('./routes/api');
 
 app.use(
   cors({
@@ -18,9 +17,7 @@ app.use(morgan('combined'));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
-
-app.use('/planets', planetsRouter);
-app.use('/launches', launchesRouter);
+app.use('/v1', api);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
