811d49f66ea48a9ae7a4d0c4a1914d9014a49a2b
vitoorsouzaa1 <vitor.souza407@yahoo.com.br>
2022-05-29 00:08:13 -0300

    feat: add new test features to front-end

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,19 +1,18 @@
 const request = require('supertest');
 const app = require('../../app');
-const { loadPlanetsData } = require('../../models/planets.model');
 const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
+const { loadPlanetsData } = require('../../models/planets.model');
 
 describe('Launches API', () => {
   beforeAll(async () => {
     await mongoConnect();
-    await loadPlanetsData;
+    await loadPlanetsData();
   });
 
   afterAll(async () => {
     await mongoDisconnect();
   });
 
-  // Testing GET method
   describe('Test GET /launches', () => {
     test('It should respond with 200 success', async () => {
       const response = await request(app)
@@ -23,13 +22,12 @@ describe('Launches API', () => {
     });
   });
 
-  // Testing POST method
-  describe('Test POST /launches', () => {
+  describe('Test POST /launch', () => {
     const completeLaunchData = {
       mission: 'USS Enterprise',
       rocket: 'NCC 1701-D',
       target: 'Kepler-62 f',
-      launchDate: 'January 4, 2029',
+      launchDate: 'January 4, 2028',
     };
 
     const launchDataWithoutDate = {
@@ -45,7 +43,7 @@ describe('Launches API', () => {
       launchDate: 'zoot',
     };
 
-    test('It should respond with a 201 created', async () => {
+    test('It should respond with 201 created', async () => {
       const response = await request(app)
         .post('/v1/launches')
         .send(completeLaunchData)
@@ -70,6 +68,7 @@ describe('Launches API', () => {
         error: 'Missing required launch property',
       });
     });
+
     test('It should catch invalid dates', async () => {
       const response = await request(app)
         .post('/v1/launches')
